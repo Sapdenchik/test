d9c970a15b192bfb238abdd22cf3bf01ae1eecab
denis <denis.sapunov@rigelstorm.com>
2024-11-17 16:52:10 +0300

    add 2 new files

--- test.py
+++ test.py
@@ -1,3 +1,8 @@
 print('hello world')
 print('hello world')
 print('i love python')
+print('i love python')
+print('i love python')
+print('i love python')
+print('i love python')
+print('i love python')
